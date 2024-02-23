a6f9198b64df866ff953df7e3febf6747970c108
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-02-23 00:00:16 -0800

    Add files via upload

---
+++ 04_Food and Restaurant/assets/js/main.js
@@ -0,0 +1,20 @@
+
+// Scrollbar navbar 
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function (){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+// nav hide  
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
