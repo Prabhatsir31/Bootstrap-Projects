3234b265509f2e613f5ffb17d8951629ef86f484
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-03-10 10:23:52 -0700

    Add files via upload

---
+++ 07_Ditigal Marketing/assets/js/main.js
@@ -0,0 +1,44 @@
+// Header Scroll
+
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+// Owl Carousel
+$(document).ready(function() {
+    $(".client-slider-section").owlCarousel({
+        items: 4,
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+});
+
