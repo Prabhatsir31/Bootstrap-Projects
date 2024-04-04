46d5ee77b7f1f83334d2a5c133864ce6025f2b1e
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-04-04 22:00:01 +0530

    Delete 15_Best Vision/assets/js/main.js

--- 15_Best Vision/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// Header Scroll
-let nav = document.querySelector(".header");
-window.onscroll = function () {
-  if (document.documentElement.scrollTop > 50) {
-    nav.classList.add("header-scrolled");
-  } else {
-    nav.classList.remove("header-scrolled");
-  }
-};
