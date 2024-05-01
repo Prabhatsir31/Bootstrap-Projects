5009b0429b9e86f46e72de9624e77f9b300942fa
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-01 11:28:40 +0530

    Add files via upload

---
+++ 16_Music Player/assets/js/music-list.js
@@ -0,0 +1,44 @@
+let allMusic = [
+  {
+    name: "Enna Sona - Ok Janu",
+    artist: "Arjit Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Suno na Sangemarmar – Youngistan",
+    artist: "Arjit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Mohabbat Barsa De - Creature 3D",
+    artist: "Surveen Chawla",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Bekhayali - Kabir Singh",
+    artist: "Arjit Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Ik Vaari Aa - Raabta",
+    artist: "Pritam, Arijit Singh",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Hamari Adhuri Kahani - Hamari Adhuri Kahani",
+    artist: "Arjit Singh",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Pehle Bhi Mein - Animal",
+    artist: "Arjit Singh",
+    img: "music-7",
+    src: "music-7"
+  },
+];
